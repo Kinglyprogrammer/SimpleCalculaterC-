5f1c8a8215e8c90f0bb6a6a47d05f6ca0f76777b
Kinglyprogrammer <alzobairbnoal3awam@gmail.com>
2026-04-06 14:33:05 +0100

    Update CmToInch.cpp

--- CmToInch.cpp
+++ CmToInch.cpp
@@ -1,1 +1,10 @@
-Add cm to inch converter
+#include<iostream>
+using namespace std;
+int main(){
+	double Value;
+	cout<<"HeLlO uSeR i WiSh YoU aRe FiNe\n";
+	cout <<"Enter a the value with cm:\n";
+	cin>>Value;
+	const double NewValue=Value/2.54;
+	cout<<"The value with inche is:\n"<<NewValue;
+	return 0;}
